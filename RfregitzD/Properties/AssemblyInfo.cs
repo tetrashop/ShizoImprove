14fe04ae6b55ef8018f1e36a221d199675805f16
ScienceLover <ScienceLover@SCIENCELOVER-PC>
2018-12-08 22:40:33 +0330

    Boundry Condition of Tree Construction 50% Solved.

--- RfregitzD/Properties/AssemblyInfo.cs
+++ RfregitzD/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.13.16.352")]
-[assembly: AssemblyFileVersion("10.13.16.352")]
+[assembly: AssemblyVersion("10.13.16.353")]
+[assembly: AssemblyFileVersion("10.13.16.353")]
