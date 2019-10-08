56c2f98270f53f283740ffea0d0e192f511f25e6
tetrashop <ramin.edjlal1359@gmail.com>
2019-10-09 00:20:33 +0330

    Assembly

--- Refrigtz/Properties/AssemblyInfo.cs
+++ Refrigtz/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("15.56.88.455")]
-[assembly: AssemblyFileVersion("15.56.88.455")]
+[assembly: AssemblyVersion("15.56.88.456")]
+[assembly: AssemblyFileVersion("15.56.88.456")]
 [assembly: NeutralResourcesLanguageAttribute("fa")]
