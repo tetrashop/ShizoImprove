33505bc7be5a5108bb3f34ba86ad9a57a751c7e1
tetrashop <ramin.edjlal1359@gmail.com>
2019-12-02 15:02:13 +0330

    Some Bugs of Elipse fixed!

--- Ellipse/Ellipse/Ellipse/EllipsAroundFormula.cs
+++ Ellipse/Ellipse/Ellipse/EllipsAroundFormula.cs
@@ -41,7 +41,7 @@ namespace Ellipse
             double r = p * (1 / (1 + e * Math.Cos(teta)));
 
             //Second Integral priciple first parameters 
-            Laround1 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
+            Laround1 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) * teta - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
             /*  around1 = (2 * p) * (teta * Math.Log(1 + e * Math.Cos(u), Math.E) + (2 / Math.Sqrt(1 - Math.Pow(e, 2))) * teta * Math.Sin(teta) * Math.Atan(Math.Sqrt((1 - e) / (1 + e)) * Math.Tan(teta / 2)) + ((2 * e) / (Math.Sqrt(1 - Math.Pow(e, 2)))) * Math.Cos(teta) * Math.Atan(Math.Sqrt((1 - e) / (1 + e)) * Math.Tan(teta / 2)) - (2 / (1 - ((1 - e) / (1 + e)))) * ((2 * e) / (Math.Sqrt(1 - Math.Pow(e, 2)))) * teta + Math.Sqrt((1 - e) / (1 + e)) * ((2 * e) / (Math.Sqrt(1 - Math.Pow(e, 2)))) * ((2 * (1 + ((1 - e) / (1 + e)))) / (1 - ((1 - e) / (1 + e)))) * Math.Atan(Math.Sqrt((1 - e) / (1 + e)) * Math.Tan(teta / 2)) - ((2 * e) / (Math.Sqrt(1 - Math.Pow(e, 2)))) * ((Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 5) - 5 * Math.Sqrt((1 - e) / (1 + e))) / ((1 - (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 4))) * (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2) + 1))) * Math.Pow(teta, 2) * Math.Sin(teta) - (((2 * e) / (Math.Sin(1 - Math.Pow(e, 2)))) * ((10 * (Math.Sqrt((1 - e) / (1 + e))) - (2 * Math.Pow((Math.Sqrt((1 - e) / (1 + e))), 5))) / ((1 - (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 4))) * (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2) + 1)))) * Math.Cos(teta) - ((2 * e) / Math.Sqrt(1 - Math.Pow(e, 2))) * (((4 * (Math.Sqrt((1 - e) / (1 + e))) - 4 * Math.Pow((Math.Sqrt((1 - e) / (1 + e))), 3))) / (Math.Pow(1 - (Math.Pow((Math.Sqrt((1 - e) / (1 + e))), 2)), 2) * Math.Pow(1 + (Math.Pow((Math.Sqrt((1 - e) / (1 + e))), 2)), 2))) * Math.Pow(teta, 2) * Math.Atan(Math.Sqrt((1 - e) / (1 + e)) * Math.Tan(teta / 2)) + ((2 * e) / (Math.Sqrt(1 - Math.Pow(e, 2)) * ((8 - (12 * (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2)))) / (3 * (Math.Pow(1 - (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2)), 2)) * ((1 + (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2)))))))) * Math.Pow(teta, 3) + teta) + Math.Pow(p / (1 + e * Math.Cos(teta)), 2) + p / (1 + e * Math.Cos(teta));
                       Paround1 = 2 * p * (u * Math.Log(1 + e * Math.Cos(u), Math.E)
                           + (2 / Math.Sqrt(1 - Math.Pow(e, 2))) * b * u * Math.Sin(u) * Math.Atan(Math.Sqrt((1 - e) / (1 + e)) * Math.Tan(u / 2))
@@ -73,13 +73,13 @@ namespace Ellipse
                   + ((2 * e) / (Math.Sqrt(1 - Math.Pow(e, 2)))) * ((8 - 12 * (Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2))) / (3 * (Math.Pow(1 - Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2), 2) * (1 + Math.Pow(Math.Sqrt((1 - e) / (1 + e)), 2))))) * Math.Pow(u, 3)
                   + 0.5 * Math.Pow(r, 2))
                   + r + teta;*/
-            Laround0 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
+            Laround0 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) * teta - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
 
 
             //Orthogonality of multiply at 4
             //  around = 4 * (Math.Sqrt(Math.Abs(around1) - Math.Abs(around0)));
-            //Laround = 4 * (Math.Sqrt(Math.Abs(Laround1)) - Math.Sqrt(Math.Abs(Laround0)));
-            Laround = 4 * (Math.Sqrt(Math.Abs(Laround1 - Laround0)));
+            Laround = 4 * (Math.Sqrt(Math.Abs(Laround1)) - Math.Sqrt(Math.Abs(Laround0)));
+           // Laround = 4 * (Math.Sqrt(Math.Abs(Laround1 - Laround0)));
             /* Paround = 4 * (Math.Sqrt(Math.Abs(around1)) - Math.Sqrt(Math.Abs(around0)));
              aroundT = 4 * (Math.Sqrt(Math.Abs(around1 - around0)));
              aroundS = 4 * (Math.Sqrt(Math.Abs(Paround1) - Math.Abs(Paround0)));
@@ -91,18 +91,18 @@ namespace Ellipse
             r = p * (1 / (1 + e * Math.Cos(teta)));
 
             //Second Integral priciple first parameters 
-            Paround1 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
+            Paround1 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) * teta - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
 
             teta =0;
             u = teta;
             r = p * (1 / (1 + e * Math.Cos(teta)));
 
             //Second Integral priciple first parameters 
-            Paround0 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
+            Paround0 = (2 * p) * (-1 * Math.Log(1 + e * Math.Cos(u), Math.E) * teta - e * u * Math.Cos(u) - 0.5 * Math.Sin(u) + (e / 2) * Math.Log(Math.Pow(1 + e * Math.Cos(u), 2))) + Math.Pow(r, 2) + teta;
 
-            //around = Laround + 4 * (Math.Sqrt(Math.Abs(Paround1)) - Math.Sqrt(Math.Abs(Paround0)));
+            around = Laround + 4 * (Math.Sqrt(Math.Abs(Paround1)) - Math.Sqrt(Math.Abs(Paround0)));
 
-            around = Laround + 4 * (Math.Sqrt(Math.Abs(Paround1 - Paround0)));
+            //around = Laround + 4 * (Math.Sqrt(Math.Abs(Paround1 - Paround0)));
 
         }
         public double aroundAccess
