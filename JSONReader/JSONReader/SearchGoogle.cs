bedd46472730873ec0e7bca78ba8b788cf628a66
tetrashop <ramin.edjlal1359@gmail.com>
2019-12-13 20:51:27 +0330

    JsonReader hat initiated!

---
+++ JSONReader/JSONReader/SearchGoogle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace JSONReader
+{
+    class SearchGoogle
+    {
+        public DateTime timestamp_usec;
+        public String query_text = "";
+    }
+}
