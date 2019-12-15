e56975ab6193d49c0cf9a81fca47ec3e5d4f5c46
tetrashop <ramin.edjlal1359@gmail.com>
2019-12-15 23:39:06 +0330

    Revert "Refrigitz Serialization fixed!"
    This reverts commit f4e2885689567617801bdef0f376addc728ed1fe.

--- WebApplicationRefregitzTow/AllDraw.cs
+++ WebApplicationRefregitzTow/AllDraw.cs
@@ -162,9 +162,8 @@ namespace RefrigtzW
     {   //Initiate Variables. 
         bool[] ThinkingAllowed = { false, false, false, false, false, false, false, false, false, false, false, false };
 
-        [field: NonSerialized] List<Task> tH = new List<Task>();
-        [field: NonSerialized] List<Task> TH = new List<Task>();
-
+        List<Task> tH = new List<Task>();
+        List<Task> TH = new List<Task>();
         public static bool Deeperthandeeper = false;
 
         public static bool FirstTraversalTree = true;
@@ -14928,7 +14927,7 @@ namespace RefrigtzW
                 DummyOrder = Order;
                 int DummyCurrentOrder = new int();
                 DummyCurrentOrder = ChessRules.CurrentOrder;
-               // List<Task> ThB = new List<Task>();
+                List<Task> ThB = new List<Task>();
 
                 int i = 0, ik = 0;
                 int[,] TablInit = new int[8, 8];
